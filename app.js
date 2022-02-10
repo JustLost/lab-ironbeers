c6c427fc8279207c386ad797c28a5b777699f67c
Rita Cunha <rita.cunha.ts@gmail.com>
2022-02-10 11:49:57 +0000

    missing some css

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
 
 // Add the route handlers here:
 
@@ -22,4 +19,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {  
+    punkAPI.getBeers()
+      .then(beersFromApi => 
+        res.render('beers', {beersFromApi})
+      )
+      .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => res.render('random-beer', { responseFromApi }));
+  
+});
+
+app.listen(3001, () => console.log('🏃‍ on port 3000'));
